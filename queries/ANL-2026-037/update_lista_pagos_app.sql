939de01009d6c8d82f5dbbbfa0a3993e1b08c742
ronybecerra-ai <rony.becerra@rimac.com.pe>
2026-09-03 17:37:25 -0500

    Create update_lista_pagos_app.sql

---
+++ queries/ANL-2026-037/update_lista_pagos_app.sql
@@ -0,0 +1,3 @@
+insert into    `rs-prd-dlk-sbx-digt-d12c.desa_ingesta.lista_pago_app`
+ (CODPROD, RAMO, PROD)
+values ('8309','VIDA','VIDA AHORRO GARANTIZADO'), ('8820','VIDA','PROTECCION AHORRO II'),('8151','VIDA','VIDA ENDOSABLE')
