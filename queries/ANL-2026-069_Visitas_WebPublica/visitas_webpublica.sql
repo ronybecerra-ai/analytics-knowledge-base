22b55aab856fc560a1a5aed062ef554451964ae4
ronybecerra-ai <rony.becerra@rimac.com.pe>
2026-09-04 13:21:33 -0500

    Update visitas_webpublica.sql

--- queries/ANL-2026-069_Visitas_WebPublica/visitas_webpublica.sql
+++ queries/ANL-2026-069_Visitas_WebPublica/visitas_webpublica.sql
@@ -6,6 +6,6 @@
     from `rs-shr-al-analyticsz-prj-ebc1.anl_digital.registro_global` --se usa global porque consulta soat esta en solicitudes
     where (des_pagina_visitada like 'web:%')  -- se filtra lo que incluye web
     or (des_pagina_visitada like 'sol:consulta-soat%') -- se considera consulta soat
-    and periodo between '2021-01-01' and '2025-11-01' -- cambiar periodo
+    and periodo between '2021-01-01' and '2026-08-01' -- cambiar periodo
     and des_flujo_usuario = 'External' -- filtro external de adobe
     group by all
